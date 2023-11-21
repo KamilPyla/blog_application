2847c228104cc3cc624bf968f8f56e42d72af884
kamil <kamilpyla@gmail.com>
2023-11-21 22:38:15 +0100

    models services controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,73 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = PostBuilder.new(
+      current_user,
+      post_params.merge(form.attributes)
+    ).perform
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def form
+    PostForm.new(post_params)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def permitted_params
+    %i[title post_category_id body image]
+  end
+
+  def post_params
+    params.fetch(:post, {}).permit(*permitted_params, pictures: [])
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :post_category
+
+  has_one_attached :image
+  has_many_attached :pictures
+  has_rich_text :body
+
+  def image_as_thumbnail
+    return unless image.content_type.in?(%w[image/jpeg image/png])
+
+    image.variant(resize_to_limit: [300, 300]).processed
+  end
+
+  def picture_as_thumbnail(picture)
+    picture.variant(resize_to_limit: [300, 300]).processed
+  end
 end
