0444830183c6caf6beb437c01e486fc3bfa29745
kamil <kamilpyla@gmail.com>
2023-12-24 14:30:53 +0100

    views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,24 @@ module ApplicationHelper
   def object_author?(object)
     object.user_id == current_user.id
   end
+
+  def like_button(object)
+    if object.liked?(current_user)
+      button_to(
+        'Podoba Ci się', 
+        dislike_activity_path(kind: object.class.to_s, id: object.id),
+        class: 'btn btn-primary',
+        method: :delete,
+        data: { turbo: false }
+      )
+    else
+      button_to(
+        'Podoba mi się!',
+        like_activity_path(kind: object.class.to_s, id: object.id),
+        class: 'btn btn-outline-primary',
+        method: :post,
+        data: { turbo: false }
+      )
+    end
+  end
 end
